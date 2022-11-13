f7b2faaf43c6d4bcd086fb2cc00db1ed60dcd439
Vanese <vanesebuchanan@gmail.com>
2022-11-13 02:22:46 -0500

    updated routes

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const express = require("express");
+const express = require('express');
 const session = require('express-session');
-const exphbs = require("express-handlebars");
-const path = require("path");
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 
-app.listen(PORT, () => {
-     console.log("Server listening on port 3001");
-  });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
